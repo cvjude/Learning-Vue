d434dbbff6dc48f7a516637d8df84cf698564e71
Jude Chinoso <jchinoso@theagromall.com.ng>
2020-03-06 22:19:01 +0100

    Learned vue cli - learned components - learned props - learned life cycle methods

---
+++ src/components/Ninjas.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="ninjas">
+    <ul>
+      <li v-for="(ninja, index) in ninjas" v-bind:key="index" v-on:click="ninja.show = !ninja.show">
+        <h2>{{ninja.name}}</h2>
+        <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
+      </li>
+    </ul>
+    <button v-on:click="deleteNinja">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ninjas: {
+      type: Array
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    deleteNinja: function() {
+      this.ninjas.pop();
+    }
+  },
+  beforeCreate() {
+    alert("Before Create");
+  },
+  created() {
+    alert("Created");
+  },
+  beforeMount() {
+    alert("Before Mount");
+  },
+  mounted() {
+    alert("Mounted");
+  },
+  beforeUpdate() {
+    alert("Before Update");
+  },
+  updated() {
+    alert("Updated");
+  }
+};
+</script>
+
+<style scoped >
+#ninjas {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+</style>
